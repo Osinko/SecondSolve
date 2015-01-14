f078790be19918f6b9ae5d7d2a687e432264ca03
Osinko <yoshinari_m@msn.com>
2015-01-15 00:57:14 +0900

    no message

---
+++ Assets/SecondSolve1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondSolve1 : MonoBehaviour
+{
+		public int root;
+		public int  digit;
+		decimal sqrt;
+		decimal  deltaArea, restArea;
+
+		//現時点、小数点表現を抜いた27桁まで表示できる
+		void Start ()
+		{
+				sqrt = 0;
+				restArea = root;
+
+				for (int i = 0; i < digit; i++) {
+						sqrt *= 10;
+						for (int d = 1; d <= 10; d++) {
+								decimal hit = restArea;
+				
+								deltaArea = d * (sqrt * 2 + d);
+								hit = hit - deltaArea;
+								if (hit < 0) {
+										deltaArea = (d - 1) * (sqrt * 2 + (d - 1));
+										restArea = restArea - deltaArea;
+										sqrt += d - 1;
+										break;
+								}
+						}
+						restArea *= 100;
+				}
+				print (sqrt);
+		}
+}
