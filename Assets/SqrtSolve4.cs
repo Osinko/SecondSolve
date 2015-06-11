43e532a993014f52ebc01039f468589f6e7b84d4
Osinko <yoshinari_m@msn.com>
2015-06-11 21:03:44 +0900

    no message

--- Assets/SqrtSolve4.cs
+++ Assets/SqrtSolve4.cs
@@ -5,11 +5,15 @@ using System.Numerics;
 
 public class SqrtSolve4 : MonoBehaviour
 {
+		//仕様
+		//rootの値は浮動小数点6桁まで指定可
+		//digitは求める桁数。基本的に制限なし（BigIntegerの仕様に依存）
+
 		public float root;
 		public int digit;
 		Square square;
 
-		BigInteger sqrt;
+		BigInteger sqrt;					//求めた答えをここに入れている
 		BigInteger left, right;
 		BigInteger restArea, deltaArea ;	//残されたエリア、平方完成を利用して計算した膨張していく差分エリア
 	
@@ -21,6 +25,9 @@ public class SqrtSolve4 : MonoBehaviour
 				print (string.Format ("{0}　⇒　{1}　　digit:{2} ", root, square.value, square.digit));
 				int colum = square.value.ToString ().Length;
 
+				//これから計算する部分をleft
+				//計算待ちの部分をrightにしている。計算待ちの部分が無くなったら単純に余りを100倍してleftに入れる仕組みになっている
+				//最初の1回目だけ10^2の桁位置合わせの意味で奇数桁数なら1カラム。偶数桁数なら２カラムをleftに割当てるようにしている
 				string leftStr, rightStr;
 				if (colum % 2 != 0) {
 						leftStr = square.value.ToString ().Substring (0, 1);
@@ -36,8 +43,8 @@ public class SqrtSolve4 : MonoBehaviour
 				} else {
 						right = new BigInteger (rightStr);
 				}
-				print (string.Format ("{0}  {1}", left, right));
 
+				//積分計算
 				for (int i = 0; i < digit; i++) {
 						sqrt *= 10;		//平方根側の桁移動
 						for (int d = 1; d <= 10; d++) {
@@ -54,6 +61,8 @@ public class SqrtSolve4 : MonoBehaviour
 										break;
 								}
 						}
+
+						//以下はleft、rightの桁位置調整
 						BigInteger remaind = restArea * 100;
 
 						if (right == 0) {
@@ -74,20 +83,31 @@ public class SqrtSolve4 : MonoBehaviour
 				PrintSqrt (sqrt, square.digit);
 		}
 
+		//小数点表示処理（内部計算では値を整数で扱っていたので必要）
 		void PrintSqrt (BigInteger sqrt, int squareDigit)
 		{
-				squareDigit = squareDigit * -1 * 2;
-				string str = sqrt.ToString ().Insert (squareDigit, ".");
-				print (str);
+				if (squareDigit >= 0) {
+						squareDigit++;
+						string str = sqrt.ToString ().Insert (squareDigit, ".");
+						print (str);
+				} else {
+						squareDigit = squareDigit * -1 - 1;
+				
+						string dec = "0.";
+						dec = dec.PadRight (2 + squareDigit, '0');
+						string str = sqrt.ToString ().Insert (0, dec);
+						print (str);
+				}
 		}
 
+		//小数点の平方根を整数で扱えるようにするための補助クラス
 		class Square
 		{
 				public BigInteger value;
 				public int digit;
 		}
 
-		//小数点を基準に２桁ごとに区切りを入れた値　⇒　PCで計算しやすいように整数範囲へ桁移動させている
+		//小数点を基準に２桁ごとに区切りを入れた値　⇒　BigIntegerで計算出来るように整数範囲へ桁移動させている
 		Square GetInteger (float value)
 		{
 				int digit = Mathf.FloorToInt (Mathf.Log (value, 100));
@@ -102,8 +122,7 @@ public class SqrtSolve4 : MonoBehaviour
 				}
 				scale /= 2;
 				value_shift = value_shift * Mathf.Pow (100, scale);
-				//桁位置合わせ
-				digit -= scale;
+
 				return new Square{value=new BigInteger(value_shift.ToString()),digit=digit};
 		}
 
